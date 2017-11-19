24d6973754dcbeb516351cd26ed464c507574004
Tristan <tkidder132@gmail.com>
2017-11-19 01:59:44 -0600

    Imported menu package from previous projects.

---
+++ Assets/_Scripts/LevelManagerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManagerController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        //NOTE: Will not "work" in editor. 
+        Application.Quit();
+    }
+}
