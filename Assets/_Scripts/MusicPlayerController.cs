3e4873b3ee77be951be3d465f21d9acf9e4f7a6e
Tristan <tkidder132@gmail.com>
2017-12-25 03:27:29 -0600

    setup different music for each scene and added starfield background to each menu scene

--- Assets/_Scripts/MusicPlayerController.cs
+++ Assets/_Scripts/MusicPlayerController.cs
@@ -4,16 +4,53 @@ public class MusicPlayerController : MonoBehaviour
 {
     static MusicPlayerController instance = null;
 
-    void Awake()
+    public AudioClip startClip;
+    public AudioClip gameClip;
+    public AudioClip endClip;
+
+    private AudioSource music;
+
+    private void Start()
     {
-        if (instance)
+        if(instance != null && instance != this)
         {
             Destroy(gameObject);
+            print("Duplicate Music Player: Self Destructing!");
         }
         else
         {
             instance = this;
             GameObject.DontDestroyOnLoad(gameObject);
+            music = GetComponent<AudioSource>();
+            music.clip = startClip;
+            music.loop = true;
+            music.Play();
         }
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        Debug.Log("MusicPlayer: Loaded level " + level);
+        music.Stop();
+
+        switch(level)
+        {
+            case 0:
+                music.clip = startClip;
+                break;
+            case 1:
+                music.clip = gameClip;
+                break;
+            case 2:
+            case 3:
+                music.clip = endClip;
+                break;
+            default:
+                music.clip = gameClip;
+                break;
+        }
+
+        music.loop = true;
+        music.Play();
     }
 }
